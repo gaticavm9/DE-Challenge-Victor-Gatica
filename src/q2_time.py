87974327e5ccb1620ea984ae40f50d14fe071b4d
Victor Gatica <gaticavm9@gmail.com>
2024-06-21 01:15:33 -0400

    feat(pregunta-q2): Respuesta con enfoque q2_time

--- src/q2_time.py
+++ src/q2_time.py
@@ -1,4 +1,47 @@
 from typing import List, Tuple
+from datetime import datetime
+
+import polars as pl
+import emoji
+from collections import Counter
+
 
 def q2_time(file_path: str) -> List[Tuple[str, int]]:
-    pass
+    """
+    Encuentra los top 10 emojis más usados con su respectivo conteo.
+    Esta función busca optimizar el tiempo de ejecución utilizando Polars.
+
+    Args:
+        file_path (str): Ruta del JSON que contiene los datos de los tweets.
+
+    Returns:
+        List[Tuple[str, int]]: Una lista de tuplas donde cada tupla contiene un emoji y el conteo de veces que se usó.
+        [("✈️", 6856), ("❤️", 5876), ...]
+    """
+
+    df = pl.scan_ndjson(file_path)
+    df = df.select(
+        pl.col("content").apply(extract_emojis,return_dtype=pl.List(str)).alias("emojis")
+    ).filter(
+        # Filas que no tienen emojis
+        pl.col("emojis") != []
+    )
+    df = df.explode("emojis")
+    df = df.groupby("emojis").agg(pl.count().alias("count"))
+    df = df.sort("count", descending=True).limit(10)
+    df = df.collect()
+    result = list(zip(df["emojis"], df["count"]))
+    return result
+
+
+def extract_emojis(text: str) -> List[str]:
+    """
+    Extrae todos los emojis de un texto.
+
+    Args:
+        text (str): Texto del que se extraerán los emojis.
+
+    Returns:
+        List[str]: Lista de emojis extraídos.
+    """
+    return [emj['emoji'] for emj in emoji.emoji_list(text)]
